cad40ff163e31a7236197984e36fe2eabbd95ccb
RanitKallan <117113805+RanitKallan@users.noreply.github.com>
2024-02-25 17:49:47 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+const sections =  document.querySelectorAll('.section');
+const sectBtns =  document.querySelectorAll('.controlls');
+const sectBtn =  document.querySelectorAll('.control');
+const allSections =  document.querySelectorAll('.main-content');
+
+function PageTransitions() {
+    // button click active class
+    for (let i = 0; i < sectBtn.length; i++) {
+        sectBtn[i].addEventListener('click', function() {
+            let currentBtn = document.querySelector('.active-btn'); // Use querySelector to get the first match
+            if (currentBtn) {
+                currentBtn.classList.remove('active-btn'); // Use classList to remove the class
+            }
+            this.classList.add('active-btn'); // Use classList to add the class
+        });
+    }
+
+    // section active class 
+    allSections.forEach(section => {
+        section.addEventListener('click', (e) => {
+            const id = e.target.dataset.id;
+            if (id) {
+                // remove selected from the other buttons
+                sectBtn.forEach((btn) => {
+                    btn.classList.remove('active');
+                });
+                e.target.classList.add('active');
+
+                // hide other sections 
+                sections.forEach((section) => {
+                    section.classList.remove('active');
+                });
+
+                const element = document.getElementById(id);
+                if (element) {
+                    element.classList.add('active');
+                }
+            }
+        });
+    });
+
+    // toggle theme 
+    const themeBtn = document.querySelector('.theme-btn')
+    themeBtn.addEventListener('click', ()=>{
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
+}
+
+
+
+PageTransitions();
